b4e25751fc8be1443d46aa51ce42c9ca26a30b05
auste <austejajak02@gmail.com>
2024-09-19 16:40:19 +0300

    Initialized server, client communication with TCP

---
+++ BloonsServer/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BloonsServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+            public TCP(int _id)
+            {
+                this.id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallBack, null);
+            }
+
+            private void ReceiveCallBack(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(receiveBuffer, _data, _byteLength);
+
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallBack, null);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {e}");
+                }   
+            }
+        }
+    }
+}
--- BloonsServer/Program.cs
+++ BloonsServer/Program.cs
@@ -6,28 +6,10 @@ namespace BloonsServer
     {
         static void Main(string[] args)
         {
-            // Print a welcome message to the console
-            Console.WriteLine("Welcome to the Bloons Server!");
-
-            // Example of reading user input
-            Console.Write("Enter your name: ");
-            string userName = Console.ReadLine();
-            Console.WriteLine($"Hello, {userName}! The server is starting...");
-
-            // Example of server setup (this is just a placeholder)
-            SetupServer();
-
-            // Keep the console window open
-            Console.WriteLine("Press any key to exit...");
+            Console.Title = "Bloons server";
+            Server.Start(4, 26950);
             Console.ReadKey();
         }
 
-        // Example method for setting up the server
-        static void SetupServer()
-        {
-            // Placeholder for server setup logic
-            Console.WriteLine("Setting up server...");
-            // Add your server initialization code here
-        }
     }
 }
