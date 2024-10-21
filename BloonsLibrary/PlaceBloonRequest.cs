6597b8164e4fe0e339fce481060f83c1801328e8
auste <austejajak02@gmail.com>
2024-10-21 21:35:24 +0300

    Adding client starting at the same time mechanism (doesn't properly work yet)

---
+++ BloonsLibrary/PlaceBloonRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BloonLibrary
+{
+    public class PlaceBloonsRequest
+    {
+        public record PlaceBloonRequest(string TowerType, NetworkPoint2D Position, string Username);
+
+        public record SynchronizeBloon(string TowerType, NetworkPoint2D Position, string PlayerName);
+    }
+}
