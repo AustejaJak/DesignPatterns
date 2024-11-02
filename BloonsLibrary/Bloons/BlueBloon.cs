84acc5a0800199342b41d23aa75e36f73bf6442c
aleksas753 <Aleksas.Juzukonis@gmail.com>
2024-11-02 23:18:57 +0200

    prototype design pattern

--- BloonsLibrary/Bloons/BlueBloon.cs
+++ BloonsLibrary/Bloons/BlueBloon.cs
@@ -7,7 +7,20 @@ namespace BloonsProject
         public BlueBloon() : base(2, "Blue Balloon", Color.Blue, 3, 3)
         {
         }
-        
+
+        public BlueBloon(BlueBloon bloon) : base(2, "Blue Balloon", Color.Blue, 3, 3)
+        {
+            this.Position = bloon.Position;
+            this.Checkpoint = bloon.Checkpoint;
+            this.DistanceTravelled = bloon.DistanceTravelled;
+        }
+
+
         public static string Name => "Blue Balloon";
+
+        public override Bloon Clone()
+        {
+            return new BlueBloon(this);
+        }
     }
 }
--- BloonsLibrary/Bloons/RedBloon.cs
+++ BloonsLibrary/Bloons/RedBloon.cs
@@ -7,7 +7,19 @@ namespace BloonsProject
         public RedBloon() : base(1, "Red Bloon", Color.Red, 2, 2)
         {
         }
+
+        public RedBloon(RedBloon bloon) : base(1, "Red Bloon", Color.Red, 2, 2)
+        {
+            this.Position = bloon.Position;
+            this.Checkpoint = bloon.Checkpoint;
+            this.DistanceTravelled = bloon.DistanceTravelled;
+        }
         
         public static string Name => "Red Bloon";
+
+        public override Bloon Clone()
+        {
+            return new RedBloon(this);
+        }
     }
 }
