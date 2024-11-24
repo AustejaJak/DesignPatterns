dcae83c7f93fd5a1d782a88523e3ba3b915b70ca
auste <austejajak02@gmail.com>
2024-11-24 20:19:49 +0200

    Added Composite design pattern

---
+++ BloonsLibrary/Bloons/IBloon.cs
@@ -0,0 +1,10 @@
+﻿using BloonsProject;
+
+namespace BloonLibrary.Bloons
+{
+    public interface IBloon
+    {
+        void MoveBloonInDirection(Direction direction);
+        void TakeDamage(int damage);
+    }
+}
